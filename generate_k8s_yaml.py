67de2cd0ca12a125bb29ad5bfeb969e6ea98c242
Armanmalhotra <146510608+Armanmalhotra@users.noreply.github.com>
2023-09-30 11:47:50 +0530

    Update generate_k8s_yaml.py

--- generate_k8s_yaml.py
+++ generate_k8s_yaml.py
@@ -1,11 +1,14 @@
 import jinja2
-Define your template file (deployment-template.yaml)
+
+#Define your template file (deployment-template.yaml)
 template_file = 'deployment-template.yaml'
-Load the template
+
+#Load the template
 template_loader = jinja2.FileSystemLoader(searchpath="./")
 template_env = jinja2.Environment(loader=template_loader)
 template = template_env.get_template(template_file)
-Define variables from Jenkins environment
+
+#Define variables from Jenkins environment
 deployment_name = "nginx-deployment"
 replica_count = "3"
 app_name = "web"
@@ -17,7 +20,8 @@ cpu_limit = "0.5"
 memory_limit = "256Mi"
 env_variable_name = "version"
 env_variable_value = "1.0"
-Render the template with variables
+
+#Render the template with variables
 rendered_template = template.render(
   DEPLOYMENT_NAME=deployment_name,
   REPLICA_COUNT=replica_count,
@@ -31,6 +35,7 @@ rendered_template = template.render(
   ENV_VARIABLE_NAME=env_variable_name,
   ENV_VARIABLE_VALUE=env_variable_value,
 )
-Write the rendered template to deployment.yaml
+
+#Write the rendered template to deployment.yaml
 with open('k8s-deployment.yaml', 'w') as deployment_file:
   deployment_file.write(rendered_template)
